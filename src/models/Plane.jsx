254f355e20c42f3b460f6d0f7d0c206aadc1bb15
EteoclesDinah <kamalaraidanuwar01@gmail.com>
2025-07-11 23:14:10 +0545

    feat(island): add plane component.

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useGLTF } from '@react-three/drei';
+import planeScene from '../assets/3d/plane.glb';
 
 const Plane = () => {
+    const { scene, animations} = useGLTF(planeScene);
   return (
-    <mesh></mesh>
+    <mesh>
+        <primitive object={scene}/>
+    </mesh>
   )
 }
 
