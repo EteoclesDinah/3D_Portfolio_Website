ef399348d5a7fd5619d82fd24b1161a5ba31ed78
EteoclesDinah <kamalaraidanuwar01@gmail.com>
2025-07-17 13:34:19 +0545

    fix: removed footer from Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,25 @@ const App = () => {
         <Router>
             <Navbar />
             <Routes>
-                <Route path='/' element={<Home />}></Route>
-                <Route path='/about' element={<About />}></Route>
-                <Route path='/projects' element={<Projects />}></Route>
-                <Route path='/contact' element={<Contact />}></Route>
+              <Route path='/' element={<Home />}/>
+              <Route
+                path='/*'
+                element = {
+                  <>
+                    <Routes>
+                      <Route path='/' element={<Home />}></Route>
+                      <Route path='/about' element={<About />}></Route>
+                      <Route path='/projects' element={<Projects />}></Route>
+                      <Route path='/contact' element={<Contact />}></Route>
+                    </Routes>
+
+                     <Footer />
+                  </>
+                }
+              ></Route>
             </Routes>
 
-            <Footer />
+           
         </Router>
 
     </main>
