669ec6c8721e67127a1188f4b0cd5c7fed2e5cf2
EteoclesDinah <kamalaraidanuwar01@gmail.com>
2025-07-22 11:54:51 +0545

    chore: update app.jsx with simplified routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,39 @@
 import React from 'react'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar';
-
-import {Home, About, Projects, Contact} from './pages';
 import Footer from './components/Footer';
+
+import { Home, About, Projects, Contact } from './pages';
+
+const AppContent = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+
+      {/* Show footer only if not on home */}
+      {!isHomePage && <Footer />}
+    </>
+  );
+};
 
 const App = () => {
   return (
     <main className='bg-slate-300/20 h-full'>
-        <Router>
-            <Navbar />
-            <Routes>
-              <Route path='/' element={<Home />}/>
-              <Route
-                path='/*'
-                element = {
-                  <>
-                    <Routes>
-                      <Route path='/' element={<Home />}></Route>
-                      <Route path='/about' element={<About />}></Route>
-                      <Route path='/projects' element={<Projects />}></Route>
-                      <Route path='/contact' element={<Contact />}></Route>
-                    </Routes>
-
-                     <Footer />
-                  </>
-                }
-              ></Route>
-            </Routes>
-
-           
-        </Router>
-
+      <Router>
+        <AppContent />
+      </Router>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
