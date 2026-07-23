3ade173a31f4f95896bcb756c2e8b29486cfb15c
Kamal <kamalaraidanuwar01@gmail.com>
2026-07-23 22:25:18 +0545

    Configure Vite base for custom domain

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 
   //for loading assets
-  base: '/3D_Portfolio_Website/',    
+  base: '/',    
   
   plugins: [
     react(),
